75564f48d96227dae81a2ab68b3ed9a69147a019
YuliaMishina78 <yulia.mishina78@gmail.com>
2016-05-30 22:01:41 +0300

    Added subject filter to home body js.

---
+++ dopObrazovanieApp/static/js/home_body.js
@@ -0,0 +1,100 @@
+/**
+ * Created by Yulia on 22-May-16.
+ */
+function add_to_favorites(id) {
+    $.ajax({
+        type: 'POST',
+        url: '/',
+        data: {
+            "TeacherID": id,
+            "csrfmiddlewaretoken": csrf,
+            "operationType": "add"
+        },
+        success: function (data) {
+            if (data.status == 0) {
+                $.get("/", function (data, status) {
+                    $('#favorites').html($('#favorites', data).html());
+                });
+            }
+        },
+        error: function (xhr, status, err) {
+        },
+        complete: function (xhr, status) {
+        }
+    });
+}
+
+function filter_place(place) {
+    $.ajax({
+        type: 'GET',
+        url: '/',
+        data: {
+            "FilterPlace": place
+        },
+        success: function (data) {
+            $('div#teachers').html($('div#teachers', data).html());
+        },
+        error: function (xhr, status, err) {
+        },
+        complete: function (xhr, status) {
+        }
+    });
+}
+
+
+function SortTeachers(sortfield) {
+    $.ajax({
+        type: 'GET',
+        url: '/',
+        data: {
+            "SortField": sortfield
+        },
+        success: function (data) {
+            $('div#teachers').html($('div#teachers', data).html());
+        },
+        error: function (xhr, status, err) {
+        },
+        complete: function (xhr, status) {
+        }
+    });
+}
+
+function FilterSubject(sortsubject) {
+    $.ajax({
+        type: 'GET',
+        url: '/',
+        data: {
+            "FilterSubjField": sortsubject
+        },
+        success: function(data) {
+            $('div#teachers').html($('div#teachers', data).html());
+        },
+        error: function(xhr, status, err) {
+        },
+        complete: function(xhr, status) {
+        }
+    });
+}
+
+$(document).ready(function () {
+    function getCookie(c_name) {
+        if (document.cookie.length > 0) {
+            c_start = document.cookie.indexOf(c_name + "=");
+            if (c_start != -1) {
+                c_start = c_start + c_name.length + 1;
+                c_end = document.cookie.indexOf(";", c_start);
+                if (c_end == -1) c_end = document.cookie.length;
+                return unescape(document.cookie.substring(c_start, c_end));
+            }
+        }
+        return "";
+    }
+
+    $(function () {
+        $.ajaxSetup({
+            headers: {
+                "X-CSRFToken": getCookie("csrftoken")
+            }
+        });
+    });
+});
